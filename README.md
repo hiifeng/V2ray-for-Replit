8c1046354a583d263f23715082b21a658318b73c
ifeng <84112641+hiifeng@users.noreply.github.com>
2022-12-22 13:00:34 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+######################################################################
+#######                 V2ray for Replit                         #####
+####### Create By xiaowansm          Modlfy By ifeng             #####
+####### Web Site:https://www.hicairo.com                         #####
+####### Telegram:https://t.me/HiaiFeng                           #####
+######################################################################
+
+使用方法：
+点击顶部的"Run"按钮后，左侧Files菜单中，程序自动创建url.txt文件，该文件包含VMess和VLess协议的链接地址，在客户端软件中导入即可。
+同时，程序自动创建了VLess.png和VMess.png文件，分别是VLess和VMess协议的二维码，使用手机扫描即可添加节点。
+
+更换节点中的UUID（用户ID）：
+1、点击左侧Tools菜单中的Shell按钮，打开Shell窗口，运行create_UUID.sh脚本，程序会在左侧Files菜单中，创建uuid.txt文件，该文件中包含有新生成的UUID。
+2、重启应用后生效，同时新生成的节点链接包含在url.txt文件中。
+3、也可以使用第三方工具（https://www.v2fly.org/awesome/tools.html）生成UUID，然后手动打开uuid.txt文件，替换文件中的内容。
+
+注意：
+服务启动，copy节点信息后，请手动删除url.txt、uuid.txt、VLess.png及VMess.png文件。否则，你的节点任何人都可以使用。
+
+问题反馈与交流：
+在使用过程中，如果遇到问题，请使用Telegram与我联系。（https://t.me/HiaiFeng）
---
+++ main.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Create By xiaowansm
+# Modlfy By ifeng
+# Web Site:https://www.hicairo.com
+# Telegram:https://t.me/HiaiFeng
+
+export PATH="~/nginx/sbin:~/v2ray/sbin:$PATH"
+
+rm -rf ~/nginx/*_temp
+
+if [ ! -d "~/nginx" ];then
+	\cp -ax .nginx ~/nginx
+fi
+if [ ! -d "~/v2ray" ];then
+	\cp -ax .v2ray ~/v2ray
+fi
+
+if [[ -f uuid.txt ]]; then {
+  read uuid < uuid.txt
+  sed -i 's/[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}/'$uuid'/g' ~/v2ray/etc/config.json
+  } else {
+  uuid="de04add9-5c68-8bab-950c-08cd5320df18"
+  }
+fi
+url=${REPL_SLUG}.${REPL_OWNER}.repl.co
+
+vmesslink=`echo -n "{\"v\":\"2\",\"ps\":\"hicairo.com\",\"add\":\"$url\",\"port\":\"443\",\"id\":\"$uuid\",\"aid\":\"0\",\"net\":\"ws\",\"type\":\"none\",\"host\":\"$url\",\"path\":\"/vmess\",\"tls\":\"tls\"}" | base64 -w 0`
+vmesslink=vmess://$vmesslink
+
+vlesslink="vless://"$uuid"@"$url":443?encryption=none&security=tls&type=ws&host="$url"&path=%2Fvless#hicairo.com"
+
+qrencode -o VMess.png $vmesslink
+qrencode -o VLess.png $vlesslink
+
+echo -e "VMess协议链接：\n$vmesslink\n\nVLess协议链接：\n$vlesslink" > url.txt
+
+v2ray -config ~/v2ray/etc/config.json >/dev/null 2>&1 &
+nginx -g 'daemon off;'
