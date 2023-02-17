0dc652d94bead8f8a9212e7d57017dae6d440770
ifeng <84112641+hiifeng@users.noreply.github.com>
2023-02-17 09:50:28 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,22 +4,22 @@
 # Web Site:https://www.hicairo.com
 # Telegram:https://t.me/HiaiFeng
 
-export PATH="~/nginx/sbin:~/v2ray/sbin:$PATH"
+export PATH="~/nginx/sbin:~/mysql/sbin:$PATH"
 
-chmod a+x .nginx/sbin/nginx .v2ray/sbin/v2ray .v2ray/sbin/v2ctl .v2ray/sbin/qrencode
+chmod a+x .nginx/sbin/nginx .mysql/sbin/mysql .mysql/sbin/qrencode
 
 if [ ! -d "~/nginx" ];then
 	\cp -ax .nginx ~/nginx
 fi
-if [ ! -d "~/v2ray" ];then
-	\cp -ax .v2ray ~/v2ray
+if [ ! -d "~/mysql" ];then
+	\cp -ax .mysql ~/mysql
 fi
 
 UUID=${UUID:-$REPL_ID}
 VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
 
-sed -i "s#[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}#$UUID#g;s#/vmess#$VMESS_WSPATH#g;s#/vless#$VLESS_WSPATH#g" ~/v2ray/etc/config.json
+sed -i "s#[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}#$UUID#g;s#/vmess#$VMESS_WSPATH#g;s#/vless#$VLESS_WSPATH#g" ~/mysql/etc/config.json
 sed -i "s#/vmess#$VMESS_WSPATH#g;s#/vless#$VLESS_WSPATH#g" ~/nginx/conf/conf.d/default.conf
 
 URL=${REPL_SLUG}.${REPL_OWNER}.repl.co
@@ -36,5 +36,5 @@ qrencode -o - -t UTF8 $vlesslink
 
 while true; do curl -s "https://$URL" >/dev/null; echo "$(date +'%Y%m%d%H%M%S') Keeping online ..."; sleep 300; done &
 
-v2ray -config ~/v2ray/etc/config.json >/dev/null 2>&1 &
+mysql -config ~/mysql/etc/config.json >/dev/null 2>&1 &
 nginx -g 'daemon off;'
