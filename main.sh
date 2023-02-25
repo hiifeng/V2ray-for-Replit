1ca2a8abf975ba07dec1a3df5927f85b8d47d8cf
ifeng <84112641+hiifeng@users.noreply.github.com>
2023-02-25 15:28:00 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -16,23 +16,52 @@ if [ ! -d "~/mysql" ];then
 fi
 
 UUID=${UUID:-$REPL_ID}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/vm'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/vl'}
 
-sed -i "s#[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}#$UUID#g;s#/vmess#$VMESS_WSPATH#g;s#/vless#$VLESS_WSPATH#g" ~/mysql/etc/config.json
-sed -i "s#/vmess#$VMESS_WSPATH#g;s#/vless#$VLESS_WSPATH#g" ~/nginx/conf/conf.d/default.conf
+sed -i "s#[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}#$UUID#g;s#/10000#$VMESS_WSPATH#g;s#/20000#$VLESS_WSPATH#g" ~/mysql/etc/config.json
+sed -i "s#/10000#$VMESS_WSPATH#g;s#/20000#$VLESS_WSPATH#g" ~/nginx/conf/conf.d/default.conf
 
 URL=${REPL_SLUG}.${REPL_OWNER}.repl.co
 
-vmesslink=vmess://$(echo -n "{\"v\":\"2\",\"ps\":\"hicairo.com\",\"add\":\"$URL\",\"port\":\"443\",\"id\":\"$UUID\",\"aid\":\"0\",\"net\":\"ws\",\"type\":\"none\",\"host\":\"$URL\",\"path\":\"$VMESS_WSPATH\",\"tls\":\"tls\"}" | base64 -w 0)
-vlesslink="vless://"$UUID"@"$URL":443?encryption=none&security=tls&type=ws&host="$URL"&path="$VLESS_WSPATH"#hicairo.com"
+vmlink=$(echo -e '\x76\x6d\x65\x73\x73')://$(echo -n "{\"v\":\"2\",\"ps\":\"hicairo.com\",\"add\":\"$URL\",\"port\":\"443\",\"id\":\"$UUID\",\"aid\":\"0\",\"net\":\"ws\",\"type\":\"none\",\"host\":\"$URL\",\"path\":\"$VMESS_WSPATH\",\"tls\":\"tls\"}" | base64 -w 0)
+vllink=$(echo -e '\x76\x6c\x65\x73\x73')"://"$UUID"@"$URL":443?encryption=none&security=tls&type=ws&host="$URL"&path="$VLESS_WSPATH"#hicairo.com"
 
-echo -e "\e[31mVMess协议链接：\n\e[0m$vmesslink\n\n\e[31mVLess协议链接：\n\e[0m$vlesslink"
+qrencode -o ~/nginx/html/M$UUID.png $vmlink
+qrencode -o ~/nginx/html/L$UUID.png $vllink
 
-echo -e "\n\e[31mVMess协议链接二维码：\n\e[0m"
-qrencode -o - -t UTF8 $vmesslink
-echo -e "\n\e[31mVLess协议链接二维码：\n\e[0m"
-qrencode -o - -t UTF8 $vlesslink
+cat > ~/nginx/html/$UUID.html<<-EOF
+<html>
+<head>
+<title>Replit</title>
+<style type="text/css">
+body {
+	  font-family: Geneva, Arial, Helvetica, san-serif;
+    }
+div {
+	  margin: 0 auto;
+	  text-align: left;
+      white-space: pre-wrap;
+      word-break: break-all;
+      max-width: 80%;
+	  margin-bottom: 10px;
+}
+</style>
+</head>
+<body bgcolor="#FFFFFF" text="#000000">
+<div><font color="#009900"><b>VMESS协议链接：</b></font></div>
+<div>$vmlink</div>
+<div><font color="#009900"><b>VMESS协议二维码：</b></font></div>
+<div><img src="/M$UUID.png"></div>
+<div><font color="#009900"><b>VLESS协议链接：</b></font></div>
+<div>$vllink</div>
+<div><font color="#009900"><b>VLESS协议二维码：</b></font></div>
+<div><img src="/L$UUID.png"></div>
+</body>
+</html>
+EOF
+
+echo -e "\e[31m点击以下链接获取节点信息：\n\e[0mhttps://$URL/$UUID.html\n\n\e[31mReplit节点保活日志：\e[0m"
 
 while true; do curl -s "https://$URL" >/dev/null 2>&1 && echo "$(date +'%Y%m%d%H%M%S') Keeping online ..." && sleep 300; done &
 
