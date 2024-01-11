c3681059936027f3a68285846428218acfe22e1d
ifeng <84112641+hiifeng@users.noreply.github.com>
2024-01-11 19:40:52 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,15 +4,21 @@
 # Web Site:https://www.hicairo.com
 # Telegram:https://t.me/HiaiFeng
 
+# 目前没有找到获取域名的方法，请临时手动在11行设置域名。(2024.01.11 19:00)
+# 以前的保活方法已失效，群里已有大佬解决保活问题，稍后再处理保活问题，有关保活请群里咨询。(https://t.me/HiaiFeng)
+
+#URL=${REPL_SLUG}.${REPL_OWNER}.repl.co
+URL="localhost"
+
 export PATH="~/nginx/sbin:~/mysql/sbin:$PATH"
 
 chmod a+x .nginx/sbin/nginx .mysql/sbin/mysql .mysql/sbin/qrencode
 
 if [ ! -d "~/nginx" ];then
-	\cp -ax .nginx ~/nginx
+  \cp -ax .nginx ~/nginx
 fi
 if [ ! -d "~/mysql" ];then
-	\cp -ax .mysql ~/mysql
+  \cp -ax .mysql ~/mysql
 fi
 
 UUID=${UUID:-$REPL_ID}
@@ -21,8 +27,6 @@ VLESS_WSPATH=${VLESS_WSPATH:-'/vl'}
 
 sed -i "s#[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}#$UUID#g;s#/10000#$VMESS_WSPATH#g;s#/20000#$VLESS_WSPATH#g" ~/mysql/etc/config.json
 sed -i "s#/10000#$VMESS_WSPATH#g;s#/20000#$VLESS_WSPATH#g" ~/nginx/conf/conf.d/default.conf
-
-URL=${REPL_SLUG}.${REPL_OWNER}.repl.co
 
 vmlink=$(echo -e '\x76\x6d\x65\x73\x73')://$(echo -n "{\"v\":\"2\",\"ps\":\"hicairo.com\",\"add\":\"$URL\",\"port\":\"443\",\"id\":\"$UUID\",\"aid\":\"0\",\"net\":\"ws\",\"type\":\"none\",\"host\":\"$URL\",\"path\":\"$VMESS_WSPATH\",\"tls\":\"tls\"}" | base64 -w 0)
 vllink=$(echo -e '\x76\x6c\x65\x73\x73')"://"$UUID"@"$URL":443?encryption=none&security=tls&type=ws&host="$URL"&path="$VLESS_WSPATH"#hicairo.com"
@@ -36,15 +40,15 @@ cat > ~/nginx/html/$UUID.html<<-EOF
 <title>Replit</title>
 <style type="text/css">
 body {
-      font-family: Geneva, Arial, Helvetica, san-serif;
+    font-family: Geneva, Arial, Helvetica, san-serif;
     }
 div {
-      margin: 0 auto;
-      text-align: left;
+    margin: 0 auto;
+    text-align: left;
       white-space: pre-wrap;
       word-break: break-all;
       max-width: 80%;
-      margin-bottom: 10px;
+    margin-bottom: 10px;
 }
 </style>
 </head>
@@ -61,9 +65,9 @@ div {
 </html>
 EOF
 
-echo -e "\e[31m点击以下链接获取节点信息：\n\e[0mhttps://$URL/$UUID.html\n\n\e[31mReplit节点保活日志：\e[0m"
+echo -e "\e[31m点击以下链接获取节点信息：\n\e[0mhttps://$URL/$UUID.html\e[0m"
 
-while true; do curl -s "https://$URL" >/dev/null 2>&1 && echo "$(date +'%Y%m%d%H%M%S') Keeping online ..." && sleep 300; done &
+#while true; do curl -s "https://$URL" >/dev/null 2>&1 && echo "$(date +'%Y%m%d%H%M%S') Keeping online ..." && sleep 300; done &
 
 mysql -config ~/mysql/etc/config.json >/dev/null 2>&1 &
 nginx -g 'daemon off;'
